ac5db16bc0cb2a3b2d0c33263a80d5f8085645fe
Alexander Karuzoo <kalex.codr@gmail.com>
2020-11-10 12:16:17 -0300

    criação de container

---
+++ src/Styles/Container.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  width: 70%;
+  background: gray;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`;
