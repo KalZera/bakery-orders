c53e5fb1f68182e07ee19c5a085db9a7b1f1e35a
Alexander Karuzoo <kalex.codr@gmail.com>
2020-11-11 14:40:53 -0300

    add informações pagina home

--- src/Pages/PageHome/PageHome.tsx
+++ src/Pages/PageHome/PageHome.tsx
@@ -1,7 +1,19 @@
-import React, {FunctionComponent} from "react";
+import React, { FunctionComponent } from "react";
+
+import { Container } from "Styles/Container";
+import { Content, Title, SubTitle } from "./styled";
 
 interface Props {}
 
 export const PageHome: FunctionComponent<Props> = () => {
-   return <>Home</>;
+  return (
+    <>
+      <Container>
+        <Content>
+          <Title>Seja Bem Vindo!!</Title>
+          <SubTitle>Você está logado</SubTitle>
+        </Content>
+      </Container>
+    </>
+  );
 };
---
+++ src/Pages/PageHome/styled.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin: 40px;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  font-size: 32pt;
+  font-weight: 700;
+`;
+
+export const SubTitle = styled.div`
+  display: flex;
+  font-size: 24pt;
+  font-weight: 600;
+  margin-top: 15px;
+`;
